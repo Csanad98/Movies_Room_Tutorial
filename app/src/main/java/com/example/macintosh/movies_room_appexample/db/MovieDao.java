277df168e20c3604bee898497a9abfeefa007462
Csanád <bakoscsanad@gmail.com>
2018-12-05 23:32:04 +0100

    MoviesDao completed

--- app/src/main/java/com/example/macintosh/movies_room_appexample/db/MovieDao.java
+++ app/src/main/java/com/example/macintosh/movies_room_appexample/db/MovieDao.java
@@ -1,4 +1,27 @@
 package com.example.macintosh.movies_room_appexample.db;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
 public interface MovieDao {
+
+    @Query("SELECT * FROM movie WHERE title = :title LIMIT 1")
+    Movie findMovieByTitle(String title);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Movie... directors);
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    void update(Movie director);
+
+    @Query("DELETE FROM movie")
+    void deleteAll();
+
+    @Query("SELECT * FROM movie ORDER BY title ASC")
+    LiveData<List<Movie>> getAllMovies();
 }
